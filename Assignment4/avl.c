5ea7e78a3b5864448a7307ecb6e333397e65b1f1
pt3002 <prerna_tulsiani@hotmail.com>
2022-05-08 22:05:10 +0530

    assgn4

--- Assignment4/avl.c
+++ Assignment4/avl.c
@@ -7,8 +7,6 @@ void initAVL(avl *t){
     return;
 }
 
-
-
 int height(avl t){
     if(t==NULL){
         return 0;
@@ -47,7 +45,191 @@ void insertNode(avl *t, int data){
         (*t) -> right -> parent = *t;
         (*t) -> bf = height((*t) -> left) - height((*t) -> right);
     }
+    
     return;
+}
+
+node* adjust(avl *t, int data){
+    node *p;
+    p = *t;
+    while(p && p -> data != data){
+        if(p ->data == data){
+            break;
+        }
+        if(p -> data > data){
+            p = p->left;
+        }
+        if(p -> data < data){
+            p = p->right;
+        }
+    }
+    node *pp;
+    pp = p;
+    while(pp!=NULL){
+        if(pp->bf > 1 || pp->bf < -1){
+            return pp;
+        }
+        pp = pp -> parent;
+    }
+    return NULL;
+}
+
+char* rotationtype(node *imb, int data){
+    char *ans;
+    ans = (char *)malloc(sizeof(char)*2);
+    int i = 0;
+    while(i<2){
+        if(data > imb -> data){
+            ans[i] = 'R';
+            imb = imb -> right;
+        }
+        else if(data < imb -> data){
+            ans[i] = 'L';
+            imb = imb -> left;
+        }
+        i++;
+    }
+    return ans;
+}
+
+void LL(node **imb, avl *t){
+    node *p = *imb;
+    node *pp = p -> parent;          // pp is the parent of imbalanced node
+    node *q = p ->left;            
+    node *r = q->left;
+    if(pp && pp -> left == p){
+        pp -> left = q;
+    }
+    else if(pp && pp -> right == p){
+        pp -> right = q;
+    }
+    else{
+        *t = q;
+    }
+    q -> parent = pp;
+    r -> parent = q;
+    p -> parent = q;
+    p -> left = q -> right;
+    if(q->right){
+        q -> right -> parent = p;
+    }
+    q -> left = r;
+    q -> right = p;
+    p -> bf = height(p -> left) - height(p -> right);
+    q -> bf = height(q -> left) - height(q -> right);
+    r -> bf = height(r -> left) - height(r -> right);
+    while(pp){
+        pp -> bf = height(pp -> left) - height(pp -> right);
+        pp = pp->parent;
+    }
+    return ;
+}
+
+void RR(node **imb, avl *t){
+    node *p = *imb;
+    node *pp = p -> parent;
+    node *q = p ->right ;
+    node *r = q-> right;
+    if(pp && pp -> left == p){
+        pp -> left = q;
+    }
+    else if(pp && pp -> right == p){
+        pp -> right = q;
+    }
+    else{
+        *t = q;
+    }
+    q -> parent = pp;
+    r -> parent = q;
+    p -> parent = q;
+    p -> right = q -> left;
+    if(q->left){
+        q -> left -> parent = p;
+    }
+    q -> left = p;
+    q -> right = r;
+    p -> bf = height(p -> left) - height(p -> right);
+    q -> bf = height(q -> left) - height(q -> right);
+    r -> bf = height(r -> left) - height(r -> right);
+    while(pp){
+        pp -> bf = height(pp -> left) - height(pp -> right);
+        pp = pp->parent;
+    }
+    return ;
+}
+
+void RL(node **imb, avl *t){
+    node *p = *imb;
+    node *pp = p -> parent;
+    node *q = p ->right;
+    node *r = q -> left;
+    if(pp && pp -> left == p){
+        pp -> left = r;
+    }
+    else if(pp && pp -> right == p){
+        pp -> right = r;
+    }
+    else{
+        *t = r;
+    }
+    r -> parent = pp;
+    p -> parent = r;
+    q -> parent = r;
+    p -> right = r -> left;
+    if(r->left){
+        r -> left -> parent = p;
+    }
+    q -> left = r -> right;
+    if(r-> right){
+        r -> right -> parent = p;
+    }
+    r -> left = p;
+    r -> right = q;
+    p -> bf = height(p -> left) - height(p -> right);
+    q -> bf = height(q -> left) - height(q -> right);
+    r -> bf = height(r -> left) - height(r -> right);
+    while(pp){
+        pp -> bf = height(pp -> left) - height(pp -> right);
+        pp = pp->parent;
+    }
+    return ;
+}
+
+void LR(node **imb, avl *t){
+    node *p = *imb;
+    node *pp = p -> parent;
+    node *q = p -> left;
+    node *r = q -> right;
+    if(pp && pp -> left == p){
+        pp -> left = r;
+    }
+    else if(pp && pp -> right == p){
+        pp -> right = r;
+    }
+    else{
+        *t = r;
+    }
+    r -> parent = pp;
+    q -> parent = r;
+    p -> parent = r;
+    p -> left = r -> right;
+    if(r -> right){
+        r -> right -> parent = p;
+    }
+    q -> right = r -> left;
+    if(r -> left){
+        r -> left -> parent = q;
+    }
+    r -> left = q;
+    r -> right = p;
+    p -> bf = height(p -> left) - height(p -> right);
+    q -> bf = height(q -> left) - height(q -> right);
+    r -> bf = height(r -> left) - height(r -> right);
+    while(pp){
+        pp -> bf = height(pp -> left) - height(pp -> right);
+        pp = pp->parent;
+    }
+    return ;
 }
 
 void inorder(avl t){
@@ -63,3 +245,137 @@ void inorder(avl t){
     }
     inorder(t -> right);
 }
+
+//Destroy Tree
+void Destroy_Tree(avl *t){
+
+    if(*t==NULL)
+        return;
+
+    Destroy_Tree(&((*t)->left));
+    (*t)->left = NULL;
+    Destroy_Tree(&((*t)->right));
+    (*t)->right = NULL;
+    free(*t);
+    (*t) = NULL;
+}
+
+//Remove node from a tree
+avl removeNode(avl root, int key){
+    avl p;    //Pointer for finding the node to be deleted
+    avl q;    //Pointer to find its parent
+    avl r;
+    avl rp;
+    p = root;
+    avl pp;
+
+    //Checking if tree is empty
+    if(root == NULL){
+        return NULL; 
+    }
+
+    //Locating the node to be deleted and its parent
+    while(p){
+        if(p->data == key){
+            break;
+        }
+
+        q=p;
+
+        if(p->data > key){
+            p = p->left;
+        }
+
+        else{
+            p = p->right;
+        }
+    }
+
+    //If key to be deleted is not found
+    if(!p){
+        return root;
+    }
+
+    //Checking if p is the leaf node
+    if(p->left == p->right){
+        if(p == root){
+            root = NULL;   //parent node does not exist i.e. only one node is there in this tree
+        }
+        else{
+            if(q->left == p){
+                q->left = NULL;
+            }
+            else{
+                q->right = NULL;
+            }
+        }
+        pp = p -> parent;
+        free(p);
+        return root;
+    }
+
+    //If p has only left sub tree
+    if(p->left != NULL && p->right == NULL){
+        if(p == root){
+            root = p->left;
+        }
+        else{
+            if(q->left == p){
+                q->left = p->left;
+            }
+            else{
+                q->right = p->left;
+            }
+        }
+        pp = p -> parent;
+        free(p);
+        return root;
+    }
+
+    //If p has only right sub tree
+    if(p->right != NULL && p->left == NULL){
+        if(p == root){
+            root = p->right;
+        }
+        else{
+            if(q->left = p){
+                q->left = p->right;
+            }
+            else{
+                q->right = p->right;
+            }
+        }
+        pp = p -> parent;
+        free(p);
+        return root;
+    }
+
+    //If p has both LST AND RST
+    if(p->right != NULL && p->left != NULL){
+        r = p->left;
+        rp = NULL;
+        while(r->right){
+            rp = r;
+            r = r->right;
+        }
+
+        p->data = r->data;
+        if(rp){
+            rp ->right = r->left;
+        }
+        else{
+            p->left = r->left;
+        }
+        pp = r -> parent;
+        free(r);
+        return root;
+    }
+
+    while(pp){
+        pp -> bf = height(pp -> left) - height(pp -> right);
+        
+        pp = pp->parent;
+    }
+
+}
+
--- Assignment4/main.c
+++ Assignment4/main.c
@@ -1,22 +1,147 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "avl.h"
+#include<string.h>
+
+int monthfind(char *m){
+    if(strcmp(m, "January")==0){
+        return 1;
+    }
+    else if(strcmp(m, "February")==0){
+        return 2;
+    }
+    else if(strcmp(m, "March")==0){
+        return 3;
+    }
+    else if(strcmp(m, "April")==0){
+        return 4;
+    }
+    else if(strcmp(m, "May")==0){
+        return 5;
+    }
+    else if(strcmp(m, "June")==0){
+        return 6;
+    }
+    else if(strcmp(m, "July")==0){
+        return 7;
+    }
+    else if(strcmp(m, "August")==0){
+        return 8;
+    }
+    else if(strcmp(m, "September")==0){
+        return 9;
+    }
+    else if(strcmp(m, "October")==0){
+        return 10;
+    }
+    else if(strcmp(m, "November")==0){
+        return 11;
+    }
+    else if(strcmp(m, "December")==0){
+        return 12;
+    }
+}
+
+void afterinsert(avl *t, int data){
+    node *imb;
+    node *imbp; // parent of imbalanced node
+    imb = adjust(t,data);
+    while(imb!=NULL){
+        imbp = imb -> parent;
+        if(imb -> bf > 1 || imb -> bf < -1){
+            printf("--------Imbalanced Node - %d-------------\n",imb->data);
+            printf("Type of rotation - %s -  on node - %d\n",rotationtype(imb,data),imb->data);
+            char *a = rotationtype(imb,data);
+            if(a[0]=='L' && a[1]=='L'){
+                LL(&imb, t);
+            }
+            else if(a[0]=='R' && a[1] == 'R'){
+                RR(&imb, t);
+            }
+            else if(a[0]=='R' && a[1] == 'L'){
+                RL(&imb, t);
+            }
+            else if(a[0]=='L' && a[1] == 'R'){
+                LR(&imb, t);
+            }
+        }
+        imb = imbp;
+    }
+}
 
 int main(){
     avl t1;
+    node *imb;
 
     //Initialize avl tree
     initAVL(&t1);
+    while(1){
+        char *month;
+        month = (char *)malloc(sizeof(char)*20);
+		printf("Enter name or none to exit- ");
+		scanf("%s",month);
+        if(month[0]=='n'){
+            break;
+        }
+        else{
+            int x = monthfind(month);
+            printf("Month - %s -- %d\n",month,x);
+            insertNode(&t1, x);
+            afterinsert(&t1 ,x);
+            inorder(t1);
+            printf("\n");
+        }
+    }
+    while(1){
+        int i; 
+        printf("Do you want to delete any node ? otherwise enter 0 - ");
+        scanf("%d",i);
+        if(i ==0){
+            break;
+        }
+        else{
+            t1 = removeNode(t1, i);
+            
+        }
+    }
 
     //Add new node
-    insertNode(&t1, 7);
-    insertNode(&t1, 20);
-    insertNode(&t1, 5);
-    insertNode(&t1, 15);
-    insertNode(&t1, 10);
-    insertNode(&t1, 4);
-    insertNode(&t1, 33);
-    insertNode(&t1, 2);
+    // insertNode(&t1, 1);
+    // afterinsert(&t1, 1);
+    // inorder(t1);
+    // printf("|||||||||||||||||||||||||||||||||||||\n");
+    // insertNode(&t1, 2);
+    // afterinsert(&t1, 2);
+    // inorder(t1);
+    // printf("|||||||||||||||||||||||||||||||||||||\n");
+    // insertNode(&t1, 3);
+    // afterinsert(&t1, 3);
+    // inorder(t1);
+    // printf("|||||||||||||||||||||||||||||||||||||\n");
+    // insertNode(&t1, 4);
+    // afterinsert(&t1, 4);
+    // inorder(t1);
+    // printf("|||||||||||||||||||||||||||||||||||||\n");
+    // insertNode(&t1, 5);
+    // afterinsert(&t1, 5);
+    // inorder(t1);
+    // printf("|||||||||||||||||||||||||||||||||||||\n");
+    // insertNode(&t1, 6);
+    // afterinsert(&t1, 6);
+    // inorder(t1);
+    // printf("|||||||||||||||||||||||||||||||||||||\n");
+    // insertNode(&t1, 7);
+    // afterinsert(&t1, 7);
+    // inorder(t1);
+    // printf("|||||||||||||||||||||||||||||||||||||\n");
+    // insertNode(&t1, 8);
+    // afterinsert(&t1, 8);
+    // inorder(t1);
+    // printf("|||||||||||||||||||||||||||||||||||||\n");
+    // insertNode(&t1, 50);
+    // afterinsert(&t1, 50);
+    // inorder(t1);
+    Destroy_Tree(&t1);
     inorder(t1);
     return 0;
 }
