ef019edf96c6839dd652a3050911ed286ed10a39
pt3002 <prerna_tulsiani@hotmail.com>
2022-05-17 01:01:25 +0530

    assgn5

--- Assignment5/main.c
+++ Assignment5/main.c
@@ -4,19 +4,38 @@
 
 int main(){
 
-    FILE *fn;
-    int num,n,range;
+    heap h1; 
+    FILE *fptr;
 
-    printf("Enter number of positive integer:");
-    scanf("%d",&n);
-    printf("Enter max integer:");
-    scanf("%d",&range);
+    int *A;
+    A = (int *)malloc(sizeof(int) * 40);
 
-    fn=fopen("number.txt","w");
-    for (int i=0;i<n;++i) {
-        num=(rand()%range)+1;
-        fprintf(fn,"%d\n",num);
+    fptr = fopen("number.txt", "r");
+    if (fptr == NULL) {
+        printf("no such file.");
+        return 0;
     }
-    fclose(fn);
+
+    int num;
+    int i = 0;
+    while (fscanf(fptr, "%d", &num) == 1){
+        A[i] = num;
+        i+=1;
+    }
+    fclose(fptr);
+
+    init_heap(&h1, 40);
+
+    printf("\n");
+    for(int i=0; i<40; i++){
+        insert_heap(&h1, A[i]);
+    }
+    i = 0;
+    int k = h1.rear;
+    while(i<=k){
+        int a = del_max(&h1);
+        printf("%d - %d \n ",i+1,a);
+        i++;
+    }
     return 0;
 }
